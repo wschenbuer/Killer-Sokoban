2bd4d7045ed1a2daf2ddcf803beb5e5a0128e2cc
MitsuWindows <31452274+MitsuWindows@users.noreply.github.com>
2018-03-14 19:08:29 +0100

    aaa

--- KillerSokoban/KillerSokoban/src/KillerSokoban/makinof.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/makinof.java
@@ -9,6 +9,7 @@ public class makinof {
 	Square s1 = new Square();
 	Square s2 = new Square();
 	Square s3 = new Square();
+	Thing t = new Thing();
 	
 	w1.Move(d);
 	b1.Hitby(w1);
@@ -36,27 +37,38 @@ public class makinof {
     System.out.print("Can we remove worker1 from square? Y/N");
     String input3 = scanner.nextLine();
     if(input == 'Y' || 'y' ){
-    s1.Remove(w1);
+    s1.Remove(t);
     }
     
     System.out.print("Can we remove box from square? Y/N");
     String input4 = scanner.nextLine();
     if(input == 'Y' || 'y' ){
-    s2.Remove(b1);
+    s2.Remove(t);
     }
     
     System.out.print("Can we remove worker2 from square? Y/N");
     String input5 = scanner.nextLine();
     if(input == 'Y' || 'y' ){
-    s3.Remove(w2);
+    s3.Remove(t);
     }
     
     System.out.print("Can we remove worker2 from square? Y/N");
     String input6 = scanner.nextLine();
     if(input == 'Y' || 'y' ){
-    s3.Remove(w2);
+    s3.Remove(t);
     }
     
-    scanner.close();
-	
+    System.out.print("Can we accept worker1 from square? Y/N");
+    String input7 = scanner.nextLine();
+    if(input == 'Y' || 'y' ){
+    s3.Accept(t);
+    }
+    
+    System.out.print("Can we accept box from square? Y/N");
+    String input7 = scanner.nextLine();
+    if(input == 'Y' || 'y' ){
+    s3.Accept(t);
+    }
+
+    scanner.close();	
 }
