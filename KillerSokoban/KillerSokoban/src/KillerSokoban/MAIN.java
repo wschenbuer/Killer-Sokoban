175deec7ee9f630d1be9d70d6b34f5ecb7916467
Buer <wschenbuer@yahoo.com>
2018-03-14 17:25:57 +0100

    s

--- KillerSokoban/KillerSokoban/src/KillerSokoban/MAIN.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/MAIN.java
@@ -14,39 +14,43 @@ public class MAIN {
             
             
             System.out.println("****");
-            System.out.println("1.");
-            System.out.println("2.");
-            System.out.println("3.");
-            System.out.println("4.");
-            System.out.println("5.");
+           
             System.out.println("********************************");
-            System.out.println("Make a choice");
+            System.out.println("Make a choice (1-11)");
             choice = inp.nextInt();
             switch(choice){
-                case 1: b.createCustomer();
-                        choice = 6;
+                case 1:
                         
-                case 2: b.withDraw();
-                        break;
+                case 2: 
                     
                 case 3:
                     
-                case 4: b.Display();
-                        break;
+                case 4:
                         
                         
-                case 5: System.out.println("Do you really want to exit ?(y/n) ");
-                        String yesno = inp.next();
-                        if ((yesno.compareTo("y")==0 )||(yesno.compareTo("Y")==0 )){
-                        System.out.println("Exiting ...!");    
-                        break;
-                        } else {
-                            System.out.println("return to menu");
-                            choice =6;
-                        }
-                    
+                case 5:
+                	
+                case 6:
+                	
+                case 7:
+                	
+                case 8:
+                	
+                case 9:
+                	
+                case 10:
+                	
+                case 11:
+                        
+                case 12: break;
+            
+            
             }
-        } while (choice !=5);
+                    
+            
+	
+        } while (choice  !=12);
 	}
+	
 
 }
