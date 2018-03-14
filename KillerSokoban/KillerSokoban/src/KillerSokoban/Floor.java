90eb6238fb76ade52cb29fdd6a08255af8ce6eb2
Buer <wschenbuer@yahoo.com>
2018-03-14 16:52:37 +0100

    added Floor

---
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Floor.java
@@ -0,0 +1,15 @@
+package KillerSokoban;
+
+import java.util.ArrayList;
+
+public class Floor implements Steppable
+{
+
+	private ArrayList<Square> square = new ArrayList<Square>();
+	
+	public void GetWorker(Square s){}
+	
+	public void CreateThing(){};
+	
+	
+}
