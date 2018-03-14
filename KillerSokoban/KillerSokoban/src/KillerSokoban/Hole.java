d779de2729ab69d123eb4685ec475a158867c556
DucLee <funfox2906@gmail.com>
2018-03-14 18:30:50 +0100

    asdasd

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Hole.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Hole.java
@@ -4,11 +4,19 @@ public class Hole extends Thing {
 	
 	private boolean SwitchedOn;
 	
-	public void HitBy(Box b){}
+	public void HitBy(Box b){
+		System.out.println("[:Hole].HitBy(b)");
+	}
 	
-	public void HitBy(Worker w){}
+	public void HitBy(Worker w){
+		System.out.println("[:Hole].HitBy(w)");
+	}
 	
-	public void SwitchedOn(){}
+	public void SwitchedOn(){
+		System.out.println("[:Hole].SwitchOn(so)");
+	}
 	
-	public void SwitchedOFF(){}
+	public void SwitchedOFF(){
+		System.out.println("[:Hole].SwitchOFF(so)");
+	}
 }
--- KillerSokoban/KillerSokoban/src/KillerSokoban/Obstacle.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Obstacle.java
@@ -2,9 +2,13 @@ package KillerSokoban;
 
 public class Obstacle extends Thing {
 	
-	public void HitBy(Box b) {}
+	public void HitBy(Box b) {
+		System.out.println("[:Obstacle].HitBy(b)");
+	}
 	
-	public void HitBy(Worker w) {}
+	public void HitBy(Worker w) {
+		System.out.println("[:Obstacle].HitBy(w)");
+	}
 
 	
 	
