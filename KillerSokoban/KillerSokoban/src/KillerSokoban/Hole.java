82930b66ae02681442f7bd373dfced8443424ff1
DucLee <funfox2906@gmail.com>
2018-03-14 16:46:14 +0100

    Hole

---
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Hole.java
@@ -0,0 +1,14 @@
+package KillerSokoban;
+
+public class Hole {
+	
+	private boolean SwitchedOn;
+	
+	public void HitBy(Box b){}
+	
+	public void HitBy(Worker w){}
+	
+	public void SwitchedOn(){}
+	
+	public void SwitchedOFF(){}
+}
