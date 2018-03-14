cb2a56418ddb63ac43c49907d7df185b39f8621d
DucLee <funfox2906@gmail.com>
2018-03-14 17:01:39 +0100

    n

---
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/MAIN.java
@@ -0,0 +1,12 @@
+package KillerSokoban;
+
+public class MAIN {
+
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- KillerSokoban/KillerSokoban/src/KillerSokoban/Steppable.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Steppable.java
@@ -1,4 +1,4 @@
-package PatialClass;
+package KillerSokoban;
 
 public interface Steppable {
 	public void Step();
