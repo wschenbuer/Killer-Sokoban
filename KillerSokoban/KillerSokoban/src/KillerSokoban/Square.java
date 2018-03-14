be45e8b3010c6ffd670c32f7215447e317d197bc
Buer <wschenbuer@yahoo.com>
2018-03-14 17:49:44 +0100

    sad

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Square.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Square.java
@@ -8,7 +8,7 @@ public class Square {
 	public boolean GetObstacle (Square s){
 		return false;}
 	
-	public Square GetNeighbor(Direction d){}
+	public Square GetNeighbor(Direction d){return null;}
 	
 	public void SetNeighbor(Direction d, Square s){}
 	
