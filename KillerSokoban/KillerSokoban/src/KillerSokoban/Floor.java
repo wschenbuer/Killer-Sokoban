fbcb370e739cc6ac32eb99fa0f7b954ee152b543
Buer <wschenbuer@yahoo.com>
2018-03-14 16:54:56 +0100

    s

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Floor.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Floor.java
@@ -7,9 +7,15 @@ public class Floor implements Steppable
 
 	private ArrayList<Square> square = new ArrayList<Square>();
 	
-	public void GetWorker(Square s){}
+	public Worker GetWorker(Square s){}
 	
 	public void CreateThing(){};
 	
 	
+	
+	public void Eliminate(Box b){}
+
+	public void Step() {
+		
+	};
 }
