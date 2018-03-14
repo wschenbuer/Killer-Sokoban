f52ef2a5f32a2f92dc55a83246132b94b01bf718
ozkrleal <ozkralm10@hotmail.com>
2018-03-14 18:32:44 +0100

    Added debugging console print in Worker

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Worker.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Worker.java
@@ -2,13 +2,23 @@ package KillerSokoban;
 
 public class Worker extends Thing {
 	
-	public void Move(Direction d){}
+	public void Move(Direction d){
+		System.out.println("[:Worker].Move(" + d.toString() + ")");
+	}
 	
-	public void Die(){}
+	public void Die(){
+		System.out.println("[:Worker].Die()");
+
+	}
 	
-	public void HitBy(Box b){}
+	public void HitBy(Box b){
+		System.out.println("[:Worker].HitBy(b)");
+	}
 	
-	public void HitBy(Worker w){}
+	public void HitBy(Worker w){
+		System.out.println("[:Worker].HitBy(w)");
+
+	}
 	
 	public void CollideWith(Obstacle o){}
 }
