2608baf47dbe8dbfd3cefb390aad4484d5c56621
DucLee <funfox2906@gmail.com>
2018-03-14 16:42:34 +0100

    worker

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Box.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Box.java
@@ -1,5 +1,13 @@
 package KillerSokoban;
 
 public class Box {
-
+	public void HitBy(Worker w){}
+	
+	public void HitBy(Box b){}
+	
+	public void Step(Direction d){}
+	public Square GetSquare(){
+		return Square;
+	}
+	public void Eliminate(){}
 }
--- KillerSokoban/KillerSokoban/src/KillerSokoban/Worker.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Worker.java
@@ -1,5 +1,14 @@
 package KillerSokoban;
 
 public class Worker {
-
+	
+	public void Move(Direction d){}
+	
+	public void Die(){}
+	
+	public void HitBy(Box b){}
+	
+	public void HitBy(Worker w){}
+	
+	public void CollideWith(Obstacle o){}
 }
