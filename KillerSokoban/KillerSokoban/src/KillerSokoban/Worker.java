9a19c5e852bbd02e2c28e4f766d39a8962ea1c18
wschenbuer <wschenbuer@yahoo.com>
2018-04-12 23:12:20 +0200

    Revert "Revert "Revert "Revert "Merge branch 'master' of https://github.com/wschenbuer/SoftwareProjectLab""""
    This reverts commit b9fd0e25713f51f267d627096e6bd8edf4825b58.

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Worker.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Worker.java
@@ -3,21 +3,21 @@ package KillerSokoban;
 public class Worker extends Thing {
 	
 	public void Move(Direction d){
-		//System.out.println("[:Worker].Move(" + d.toString() + ")");
-		System.out.println("[:Worker].Move(d)");
+		
+		
 	}
 	
 	public void Die(){
-		System.out.println("[:Worker].Die()");
+		
 
 	}
 	
 	public void HitBy(Box b){
-		System.out.println("[:Worker].HitBy(b)");
+		
 	}
 	
 	public void HitBy(Worker w){
-		System.out.println("[:Worker].HitBy(w)");
+		
 
 	}
 	
