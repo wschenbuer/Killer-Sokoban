895515af1b84263c60b0fbe289fc0224a885fa5b
MitsuWindows <mitsuhiro.makino777@gmail.com>
2018-04-18 16:07:46 +0200

    Merge branch 'master' of https://github.com/wschenbuer/Killer-Sokoban
    jjj

--- KillerSokoban/KillerSokoban/src/KillerSokoban/Switch.java
+++ KillerSokoban/KillerSokoban/src/KillerSokoban/Switch.java
@@ -3,20 +3,33 @@ package KillerSokoban;
 public class Switch extends Thing {
 	
 	private int id;
-private int squareid;
+	private int squareid;
+	private boolean SwitchedOn;
 	
 	public void setsquareid(int squareid)
 	{
 		this.squareid=squareid;
 	}
+	
+	
 	public int getsquareid()
-	{return this.squareid;
+	
+	{
+		return this.squareid;
 	}
+	
 	Switch(int id)
 	{
 		this.id=id;
 	}
 	
-	public void HitBy(Box b){}
-	public void HitBy(Worker w){}
+	public void HitBy(Box b)
+	{
+		SwitchedOn = true;
+	}
+	
+	public void HitBy(Worker w)
+	{
+		SwitchedOn = false;
+	}
 }
